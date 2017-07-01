2139e177736f079b915f16085246beb6b4a26887
ninjakx <samurai.kittycat@gmail.com>
2017-07-01 21:27:26 +0530

    Add files via upload

---
+++ fbb.py
@@ -0,0 +1,39 @@
+import cookielib
+import urllib2
+import mechanize
+
+
+# Browser
+br = mechanize.Browser()
+
+# To enable cookies
+cookiejar = cookielib.LWPCookieJar()
+br.set_cookiejar( cookiejar )
+
+# Browser settings
+br.set_handle_equiv( True )
+br.set_handle_gzip( True )
+br.set_handle_redirect( True )
+br.set_handle_referer( True )
+br.set_handle_robots( False )
+
+
+br.set_handle_refresh( mechanize._http.HTTPRefreshProcessor(), max_time = 1 )
+
+br.addheaders = [ ( 'User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1' ) ]
+
+# To authenticate
+br.open("https://www.facebook.com/")
+br.select_form(nr = 0)        #first form is a login form
+
+br[ "email" ] = "your email/phone no"
+br[ "pass" ] = "your password"
+res = br.submit()
+
+br.geturl()
+
+
+print "Success!\n"
+
+
+
